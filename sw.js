60e5b22b3249d0f250740c753e95e48eb244acf1
Mati910226 <mateusz.matsp@gmail.com>
2026-06-26 17:10:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,16 +5,49 @@
 \paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
 
-\f0\fs24 \cf0 self.addEventListener("install", (e) => \{\
-  e.waitUntil(\
-    caches.open("resin-app").then((cache) => \{\
-      return cache.addAll(["./", "./index.html"]);\
+\f0\fs24 \cf0 const CACHE_NAME = "zywice-cache-v1";\
+\
+const FILES_TO_CACHE = [\
+  "./",\
+  "./index.html",\
+  "./manifest.json",\
+  "./icon.png"\
+];\
+\
+// Instalacja \'97 zapis plik\'f3w do cache\
+self.addEventListener("install", (event) => \{\
+  event.waitUntil(\
+    caches.open(CACHE_NAME).then((cache) => \{\
+      return cache.addAll(FILES_TO_CACHE);\
     \})\
   );\
+  self.skipWaiting();\
 \});\
 \
-self.addEventListener("fetch", (e) => \{\
-  e.respondWith(\
-    caches.match(e.request).then((r) => r || fetch(e.request))\
+// Aktywacja \'97 czyszczenie starego cache\
+self.addEventListener("activate", (event) => \{\
+  event.waitUntil(\
+    caches.keys().then((keys) => \{\
+      return Promise.all(\
+        keys.map((key) => \{\
+          if (key !== CACHE_NAME) \{\
+            return caches.delete(key);\
+          \}\
+        \})\
+      );\
+    \})\
+  );\
+  self.clients.claim();\
+\});\
+\
+// Fetch \'97 tryb offline-first\
+self.addEventListener("fetch", (event) => \{\
+  event.respondWith(\
+    caches.match(event.request).then((cached) => \{\
+      return (\
+        cached ||\
+        fetch(event.request).catch(() => caches.match("./index.html"))\
+      );\
+    \})\
   );\
 \});}
